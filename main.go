4507767c94badd89748f7c31884e20e4b53582e9
kohey <kawasonblog@gmail.com>
2020-07-11 22:55:01 +0900

    入力項目を受付

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var category string
+	var price int
+
+	fmt.Print("品目>")
+	fmt.Scan(&category)
+
+	fmt.Print("値段>")
+	fmt.Scan(&price)
+
+	fmt.Println("============")
+
+	fmt.Printf("%s に %d 円使いました", category, price)
+
+	fmt.Println("============")
 }
