3d300fd97325ddfadae35559aac8e6f5c1aa4a35
kohey <kawasonblog@gmail.com>
2020-07-11 23:01:52 +0900

    データを記録する

--- main.go
+++ main.go
@@ -2,19 +2,27 @@ package main
 
 import "fmt"
 
+// Item で品目と値段を扱う
+type Item struct {
+	Category string
+	Price    int
+}
+
 func main() {
-	var category string
-	var price int
+	item := inputItem()
+	fmt.Println("===========")
+	fmt.Printf("%s に %d 円使いました", item.Category, item.Price)
+	fmt.Println("===========")
+}
+
+func inputItem() Item {
+	var item Item
 
 	fmt.Print("品目>")
-	fmt.Scan(&category)
+	fmt.Scan(&item.Category)
 
 	fmt.Print("値段>")
-	fmt.Scan(&price)
+	fmt.Scan(&item.Price)
 
-	fmt.Println("============")
-
-	fmt.Printf("%s に %d 円使いました", category, price)
-
-	fmt.Println("============")
+	return item
 }
