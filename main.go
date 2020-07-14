0d4cb54fb953cab27029cc35faa4d5d5747ac3e8
kohey <kawasonblog@gmail.com>
2020-07-14 16:39:43 +0900

    不要な定義と main() を削除

--- main.go
+++ main.go
@@ -2,43 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 )
 
-// Item で品目と値段を扱う
-type Item struct {
-	Category string
-	Price    int
-}
-
+// 入力によって処理を分岐
 func main() {
-	file, err := os.Create("accountbook.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	var n int
-	fmt.Print("何件入力しますか？>")
-	fmt.Scan(&n)
-
-	// 指定された回数、入力を受け付ける
-	for i := 0; i < n; i++ {
-		if err := inputItem(file); err != nil {
-			// エラーしたら終了
-			log.Fatal(err)
-		}
-	}
-
-	// ファイルを閉じる
-	if err := file.Close(); err != nil {
-		log.Fatal(err)
-	}
-
-	// ファイルに書いてある分は、全て出力する
-	if err := showItems(); err != nil {
-		log.Fatal(err)
-	}
 }
 
 // item に 入力値を紐づけて、返す
